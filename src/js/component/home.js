f82804314cec3d0341e5c98d944a8d7800ee94f1
camillawilliams <camillawilliamsdu@gmail.com>
2021-01-13 23:40:40 +0000

    beginning creating list

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,21 +4,14 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+const todoList = ["do homework", "take out garbage", "go to the mall"]
+const [theList, listFunction] = useState
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="todoList">
+                <input className="todoInput"></input>
 		</div>
+    </div>
 	);
 }
